588d70d8d9de523e9695247867b5b6d12fbfc9c5
ph0que <ph0que@gmail.com>
2025-12-17 14:39:09 +0500

    Add CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,14 @@ import uvicorn
 
 app = FastAPI()
 
-# CORS configuration
+origins = [
+    "http://localhost:5173",
+    "https://telegram-chat-app-97tg.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
